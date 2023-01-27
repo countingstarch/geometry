57101a719f157bc3715b2e8fc3896189efc1a99d
countingstarch <121007653+countingstarch@users.noreply.github.com>
2023-01-27 18:01:39 +0700

    Add files via upload
    Added light and dark themes. This is a backup for when I inevitably royally screw up webgl

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ let unit;
 
 const door = document.querySelectorAll('.door');
 const item = document.querySelectorAll('.item');
+
+const doordiaelem = document.querySelector('#doordia');
 let doordims = 0;
+let doordia = 0;
 
 for (i of door) {
     i.addEventListener('input', () => {
         if (door[0].value && door[1].value) {
-            console.log('hello')
-            doordims = door[0].value*door[1].value
+            doordims = door[0].value * door[1].value
+            doordia = Math.sqrt(door[0].value ** 2 + door[1].value ** 2)
+
+            doordiaelem.textContent = doordia;
+        } else {
+            doordiaelem.textContent = '--';
         }
     })
 }
