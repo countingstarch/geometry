bba5aee6516bc31d8250c89b5c9fef7ba62fee3d
countingstarch <121007653+countingstarch@users.noreply.github.com>
2023-01-28 16:37:15 +0700

    Add files via upload
    The math is mathing

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ const runitNodes = document.querySelectorAll('.runit');
 let rawrunit = 'rad';
 let runit;
 
-// Reset the angle unit select to make the darn thing work after soft refresh
-runitInput.value = 'rad'
-
 // Inputs for door and item dimensions
 const door = document.querySelectorAll('.door');
 const item = document.querySelectorAll('.item');
+
+// Reset inputs to make the darn thing work after softrefresh and to make it consistent
+lunitInput.value = 'cm';
+runitInput.value = 'rad';
+for (i of door) { i.value = '' }
+for (i of item) { i.value = '' }
 
 // dia is for diagonal, dim is for dimensions
 const doordiaelem = document.querySelectorAll('.doordia');
@@ -23,14 +26,28 @@ let doordims = 0;
 let doordia = 0;
 let doorangle = 0;
 
+const itemfaceelem = document.querySelectorAll('.itemface');
+const itemfacediaelem = document.querySelectorAll('.itemfacedia');
+const itemvolelem = document.querySelectorAll('.itemvol');
+const itemvoldiaelem = document.querySelectorAll('.itemvoldia');
+/* const itemface1elem = document.querySelectorAll('.itemface.1');
+const itemface2elem = document.querySelectorAll('.itemface.2');
+const itemface3elem = document.querySelectorAll('.itemface.3');
+const itemvolelem = document.querySelectorAll('.itemvol'); */
+let itemface = [];
+let itemvol = 0;
+let itemvoldia = 0;
+
 // Will sort the values in the input boxes soon
 const doorarr = [];
 const itemarr = [];
+const itemfacearr = [];
+const itemfacediarr = [];
 
 for (i of door) {
-    // Listen to any updates on the inputs
     i.addEventListener('input', () => {
-        if (door[0].value && door[1].value && !(door[0].value == 0 && door[1].value == 0)) {
+        // Listen to any updates on the inputs, don't allow empty values
+        if (!(door[0].value === '' || door[1].value === '') && !(door[0].value === '0' && door[1].value === '0')) {
             doorarr[0] = parseInt(door[0].value);
             doorarr[1] = parseInt(door[1].value);
             doorarr.sort();
@@ -44,13 +61,46 @@ for (i of door) {
             }
 
             for (i of doordiaelem) {
-                i.textContent = doordia;
+                i.textContent = parseFloat(doordia.toPrecision(4));
             } for (i of doorangleelem) {
-                i.textContent = doorangle;
+                i.textContent = parseFloat(doorangle.toPrecision(3));
             }
         } else {
             for (i of doordiaelem) { i.textContent = '--'; }
             for (i of doorangleelem) { i.textContent = '--' }
+        }
+    })
+} for (i of item) {
+    i.addEventListener('input', () => {
+        if (!(item[0].value === '' || item[1].value === '' || item[2].value === '')) {
+            itemarr[0] = parseInt(item[0].value);
+            itemarr[1] = parseInt(item[1].value);
+            itemarr[2] = parseInt(item[2].value);
+            itemarr.sort()
+
+            itemfacearr[0] = itemarr[0] * itemarr[1];
+            itemfacearr[1] = itemarr[0] * itemarr[2];
+            itemfacearr[2] = itemarr[1] * itemarr[2];
+
+            itemfacediarr[0] = Math.sqrt(itemarr[0] ** 2 + itemarr[1] ** 2);
+            itemfacediarr[1] = Math.sqrt(itemarr[0] ** 2 + itemarr[2] ** 2);
+            itemfacediarr[2] = Math.sqrt(itemarr[1] ** 2 + itemarr[2] ** 2);
+
+            for (i of itemfaceelem) {
+                i.textContent = itemfacearr[parseInt(i.classList[1]) - 1];
+            } for (i of itemfacediaelem) {
+                i.textContent = parseFloat(itemfacediarr[parseInt(i.classList[1]) - 1].toPrecision(4));
+            } for (i of itemvolelem) {
+                i.textContent = itemarr[0] * itemarr[1] * itemarr[2];
+            } for (i of itemvoldiaelem) {
+                i.textContent = parseFloat(Math.sqrt(itemarr[2]**2+itemfacediarr[0]**2).toPrecision(4))
+            }
+        } else {
+            // note to self: experiment with this keyword later
+            for (i of itemfaceelem) { i.textContent = '--'; }
+            for (i of itemfacediaelem) { i.textContent = '--'; }
+            for (i of itemvolelem) { i.textContent = '--' }
+            for (i of itemvoldiaelem) { i.textContent = '--' }
         }
     })
 }
